5805a23c01cfe1debce1ce69e9bf1bb678c441ec
Angelica <angelicapedroso05@gmail.com>
2021-09-12 01:45:28 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,23 @@
+function pixelFrame() {
+  let size = 5;
+  let frame = document.getElementById('pixel-board');
+
+for (let index = 1; index <= size; index += 1) {
+  let line = document.createElement('div');
+  frame.appendChild(line);
+  
+  for(let index = 1; index <= size; index += 1) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    line.appendChild(pixel);
+  }
+ }
+}
+pixelFrame();
+
+
+
+
+
+
+
