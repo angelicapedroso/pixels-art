7d9c539c9542aaaf610bc505588cacc13ef58685
Angelica <angelicapedroso05@gmail.com>
2021-09-16 15:24:17 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 function pixelFrame() {
-  
+
   let size = 5;
   let frame = document.getElementById('pixel-board');
 
-for (let index = 1; index <= size; index += 1) {
-  let line = document.createElement('div');
-  frame.appendChild(line);
-  
-  for(let index = 1; index <= size; index += 1) {
-    let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    line.appendChild(pixel);
+  for (let index = 1; index <= size; index += 1) {
+    let line = document.createElement('div');
+    frame.appendChild(line);
+
+    for (let index = 1; index <= size; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      line.appendChild(pixel);
+    }
   }
- }
 }
-pixelFrame(); 
+pixelFrame();
 
-let palette = document.getElementsByClassName('color');
+const palette = document.getElementsByClassName('color');
 
-for(let index = 0; index < palette.length; index += 1) {
-    palette[index].addEventListener("click", selectColor);
+for (let index = 0; index < palette.length; index += 1) {
+  palette[index].addEventListener("click", selectColor);
 }
 
 function selectColor(origin) {
-    for (let index = 0; index < palette.length; index += 1) {
-        palette[index].classList.remove('selected');
-    }
-    origin.target.classList.add('selected');
+  for (let index = 0; index < palette.length; index += 1) {
+    palette[index].classList.remove('selected');
+  }
+  origin.target.classList.add('selected');
 }
 
+const addColorPixel = document.getElementsByClassName('pixel');
 
+for (let index = 0; index < addColorPixel.length; index += 1) {
+  addColorPixel[index].addEventListener('click', selectPixel);
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function selectPixel(origin) {
+  origin.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected'), null).getPropertyValue("background-color");
+}
